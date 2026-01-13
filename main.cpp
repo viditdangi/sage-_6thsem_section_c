a53e5e9d66b74ca715b52f917fd5b8ddae48dc87
viditdangi <viditdangi18@gmail.com>
2026-01-14 00:25:44 +0530

    programs

---
+++ main.cpp
@@ -0,0 +1,8 @@
+ #include <iostream> // Preprocessor directive to include the Input/Output stream library
+
+int main() {
+    // std::cout is used to print text to the console
+    std::cout << "Hello, World!" << std::endl; 
+    
+    return 0; // Returns 0 to indicate the program finished successfully
+}
